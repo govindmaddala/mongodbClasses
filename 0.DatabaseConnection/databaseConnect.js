25b8d6c1f3261fa008105f249b3bda8ab1d6d7ae
govindmaddala <govindmvn@gmail.com>
2023-04-20 22:57:03 +0530

    databaseConnection is modified

--- 0.DatabaseConnection/databaseConnect.js
+++ 0.DatabaseConnection/databaseConnect.js
@@ -3,10 +3,10 @@ const url = "mongodb://127.0.0.1:27017"
 
 const client = new MongoClient(url);
 
-exports.databaseConnect = async (collectionName) => {
+exports.databaseConnect = async (dbName,collectionName) => {
     await client.connect();
     console.log("Database is connected");
-    const db = client.db("dbProject");
+    const db = client.db(dbName);
     const collection = db.collection(collectionName);
     return {
         collection, client
